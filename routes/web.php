0930f9783e232d1f98bebceb8ac1c03302d69a35
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-08-11 15:03:47 +0600

    fix v13

--- routes/web.php
+++ routes/web.php
@@ -33,39 +33,9 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function()
     /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::post('/mail/send', function () {
-    if (isset($_POST['sendMailBtn'])) {
-        $toEmail = 'newsutkent@gmail.com';
-        $subjectName = 'TEsttTEST';
-        $name = $_POST['name'];
-        $phone = $_POST['phone'];
-
-        $to = $toEmail;
-        $subject = $subjectName;
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $message = '<!doctype html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport"
-                          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                    <title>Document</title>
-                </head>
-                <body>
-                <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$name.'</span>
-                    <div class="container">
-                     '.$name.'<br/>
-                        Regards<br/>
-
-                    </div>
-                </body>
-                </html>';
-        $result = @mail($to, $subject, $message, $headers);
-
+        mail('newsutkent@gmail.com', '$subject', '$message', null, 'newsutkent@sutkent.kz');
         echo '<script>alert("Email sent successfully !")</script>';
         echo '<script>window.location.href="/";</script>';
-    }
     })->name('mail.send');
     Route::get('/auyl', function () {
         return view('pages.auyl');
