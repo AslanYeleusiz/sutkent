12c582ae36ec3b8f8491d9aa6bcfe6ae076b10cc
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-08-11 14:44:24 +0600

    fix v7

--- config/mail.php
+++ config/mail.php
@@ -80,6 +80,15 @@ return [
         ],
     ],
 
+
+    'stream' => [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true,
+        ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
